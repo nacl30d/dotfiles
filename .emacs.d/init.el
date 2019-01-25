0ff5e598c9b525ded0006c1360e5394b0e8e537d
d-salt <d-salt.web+dev@gmail.com>
2019-01-25 18:46:49 +0900

    install straight.el and use-package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,17 +1,30 @@
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(setq load-path (cons "~/.emacs.d/elisp" load-path))
-
 (setq debug-on-error t)
 
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
+;;
+;; straight.el
+;;
 
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
+
+
+;;
 ;; encoding
+;;
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (set-buffer-file-coding-system 'utf-8)
@@ -19,20 +32,48 @@
 (set-default-coding-systems 'utf-8)
 (prefer-coding-system 'utf-8)
 
+;;
 ;; language
+;;
 (set-locale-environment nil)
 (set-language-environment 'utf-8)
 
-;; theme
-(load-theme 'srcery t)
+;;
+;; input method
+;;
+(use-package mozc
+             :init
+             (setq mozc-helper-program-name "mozc_emacs_helper")
+             (setq default-input-method "japanese-mozc")
+             (custom-set-variables '(mozc-leim-title "あ"))
+             :config
+             (set-cursor-color "cyan")
+             (add-hook 'input-method-activate-hook
+                       (lambda () (set-cursor-color "masenta")))
+             (add-hook 'input-method-inactivate-hook
+                       (lambda () (set-cursor-color "cyan"))))
 
-(setq inhibit-startup-message t)
+(use-package mozc-popup
+             :config
+             (setq mozc-candidate-style 'popup))
+
+;;
+;; basics
+;;
 (setq-default tab-width 4 indent-tabs-mode nil)
 (setq completion-ignore-case t)
 (global-auto-revert-mode 1)
-(global-linum-mode t)
-(setq linum-format "%d ")
-(column-number-mode t)
+(fset 'yes-or-no-p 'y-or-n-p)
+(setq backup-directory-alist            ;backup file dir
+      (cons (cons ".*" (expand-file-name "~/.emacs.d/.backup/"))
+            backup-directory-alist))
+(setq auto-save-file-name-transforms    ;auto-save file dir
+      `((".*", (expand-file-name "~/.emacs.d/.backup/") t)))
+(setq auto-save-list-file-prefix "~/.emacs.d/.backup/auto-save-list/.saves-")
+
+;;
+;; mode line
+;;
 (defface egoge-display-time
   '((((type tty))
        (:foreground "blue")))
@@ -41,84 +82,106 @@
       '((propertize (concat " " monthname " " day " " 24-hours ":" minutes " ")
                     'face 'egoge-display-time)))
 (display-time-mode t)
-(fset 'yes-or-no-p 'y-or-n-p)
+(global-linum-mode t)
+(setq linum-format "%d ")
+(column-number-mode t)
+
+
+;;
+;; version control system
+;;
 (setq vc-follow-symlinks t)
+(use-package magit
+             :bind ("C-x g". magit-status))
 
-;; highlights
-(show-paren-mode t)
-(require 'hiwin)            ;change background color if active window or not
-(hiwin-activate)
-(set-face-background 'hiwin-face "gray50")
-(require 'hl-line+)
-(global-set-key (kbd "C-x C-h") 'flash-line-highlight)
-(set-face-background 'hl-line "yellow")
 
-;; white space
-(require 'whitespace)
-(setq whitespace-style '(face tabs tab-mark spaces space-mark))
-(setq whitespace-display-mappings
-      '((space-mark ?\u3000 [?\u25a1])
-        (tab-mark ?\t [?\xBB ?\t] [?\\ ?\t])))
-(setq whitespace-space-regexp "\\(\u3000+\\)")
-(set-face-foreground 'whitespace-tab "#adff2f")
-(set-face-background 'whitespace-tab 'nil)
-(set-face-underline  'whitespace-tab t)
-(set-face-foreground 'whitespace-space "#7cfc00")
-(set-face-background 'whitespace-space 'nil)
-(set-face-bold-p     'whitespace-space t)
-(global-whitespace-mode 1)
-(global-set-key (kbd "C-x w") 'global-whitespace-mode)
+;;
+;; views
+;;
+(show-paren-mode t)                     ;highlight paren pairs
+(menu-bar-mode -1)                      ;hidden menu bar
+(setq inhibit-startup-message t)        ;hidden startup msg
+(global-set-key (kbd "C-x p") '(lambda () (interactive)(other-window -1))) ;reverse windo
 
-;; comment
-(defun one-line-comment ()
+(use-package srcery-theme               ;theme
+             :init
+             (load-theme 'srcery t))
+
+(use-package hiwin                      ;change background color if active window or not
+             :init
+             (hiwin-activate)
+             :config
+             (set-face-background 'hiwin-face "gray50"))
+
+(use-package hl-line+                   ;flash cursor line
+             :bind ("C-x C-h" . flash-line-highlight)
+             :config
+             (set-face-background 'hl-line "yellow"))
+
+(use-package whitespace                ;white spaces
+              :bind ("C-x w" . global-whitespace-mode)
+              :init
+              (global-whitespace-mode 1)
+              :config
+              (setq whitespace-style '(face tabs tab-mark spaces space-mark))
+              (setq whitespace-display-mappings
+                    '((space-mark ?\u3000 [?\u25a1])
+                      (tab-mark ?\t [?\xBB ?\t] [?\\ ?\t])))
+              (setq whitespace-space-regexp "\\(\u3000+\\)")
+              (set-face-foreground 'whitespace-tab "#adff2f")
+              (set-face-background 'whitespace-tab 'nil)
+              (set-face-underline  'whitespace-tab t)
+              (set-face-foreground 'whitespace-space "#7cfc00")
+              (set-face-background 'whitespace-space 'nil)
+              (set-face-bold-p     'whitespace-space t))
+
+
+;;
+;; edit
+;;
+(global-set-key (kbd "C-c g") 'goto-line) ;goto line
+(defun one-line-comment ()              ;toggle comment
   (interactive)
-   (save-excursion
-     (beginning-of-line)
-     (set-mark (point))
-     (end-of-line)
-     (comment-or-uncomment-region (region-beginning) (region-end))))
+  (save-excursion
+    (beginning-of-line)
+    (set-mark (point))
+    (end-of-line)
+    (comment-or-uncomment-region (region-beginning) (region-end))))
 (global-set-key (kbd "M-/") 'one-line-comment)
 
-;; auto-complete
-(require 'auto-complete)
-(ac-config-default)
-(add-to-list 'ac-modes 'text-mode)
-(add-to-list 'ac-modes 'fundamental-mode)
-(add-to-list 'ac-modes 'latex-mode)
-(ac-set-trigger-key "TAB")
-(setq ac-use-menu-map t)
-(setq ac-use-fuzzy t)
+(use-package auto-complete              ;complement
+             :init
+             (ac-config-default)
+             :config
+             (add-to-list 'ac-modes 'text-mode)
+             (add-to-list 'ac-modes 'fundamental-mode)
+             (add-to-list 'ac-modes 'latex-mode)
+             (add-to-list 'ac-modes 'markdown-mode)
+             (ac-set-trigger-key "TAB")
+             (setq ac-use-menu-map t)
+             (setq ac-use-fuzzy t)
+             (setq ac-comphist-file "~/.emacs.d/.cache/auto-complete/ac-comphist.dat"))
 
-;; backup file
-(setq backup-directory-alist
-      (cons (cons ".*" (expand-file-name "~/.emacs.d/backup/"))
-            backup-directory-alist
-      )
-      )
+(use-package undo-tree
+             :init
+             (global-undo-tree-mode))
 
-;; auto-save file
-(setq auto-save-file-name-transforms
-      `((".*", (expand-file-name "~/.emacs.d/backup/") t))
-)
+(use-package web-mode
+             :mode (("\\.html?\\'" . web-mode)
+                    ("\\.js\\'" . web-mode)
+                    ("\\.css\\'" . web-mode)
+                    ("\\.php\\'" . web-mode))
+             :config
+             (setq web-mode-enable-auto-closing t)
+             (setq web-mode-enable-auto-pairing t)
+             (setq web-mode-markup-indent-offset 2)
+             (setq web-mode-css-indent-offset 2)
+             (setq web-mode-code-indent-offset 2)
+             (setq web-mode-style-padding 1)
+             (setq web-mode-script-padding 1)
+             (setq web-mode-block-padding 0)
+             (setq web-mode-enable-css-colorization t))
 
-;; key bainds
-(global-set-key (kbd "C-x :") 'goto-line)
-
-;; assign meta key to option key on Mac
-(when (eq system-type 'darwin)
-  (setq mac-option-modifier 'meta)
-)
-
-;; change cursor color when jspanese input mode
-;; (when (fboundp 'mac-input-source)
-;;   (defun change-cursor ()
-;;     (let ((mac-input-source (mac-input-source)))
-;;       (set-cursor-color (if (string-match "\\.US$" mac-input-source) "Yellow" "Red"))
-;;     )
-;;   )
-;;   (add-hook 'mac-selected-keyboard-input-source-change-hook
-;;             'change-cursor)
-;; )
 
 ;; TeX mode
 (setq auto-mode-alist
@@ -160,21 +223,23 @@
 ;; RefTeX with TeX mode
 (add-hook 'latex-mode-hook 'turn-on-reftex)
 
-;; magit
-(global-set-key (kbd "C-x g") 'magit-status)
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (hiwin auto-complete magit markdown-mode srcery-theme))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;;
+;; shell
+;;
+(use-package multi-term
+             :bind ("C-c t" . multi-term)
+             :config
+             (setq multi-term-program "/usr/local/bin/zsh")
+             (delete "C-c" term-unbind-key-list)
+             (defun term-send-previous-line ()
+               (interactive)
+               (term-send-raw-string (kbd "C-p")))
+             (defun term-send-next-line ()
+               (interactive)
+               (term-send-raw-string (kbd "C-n")))
+             (add-hook 'term-mode-hook
+                       '(lambda ()
+                          (define-key term-raw-map (kbd "C-p") 'term-send-previous-line)
+                          (define-key term-raw-map (kbd "C-n") 'term-send-next-line))))
