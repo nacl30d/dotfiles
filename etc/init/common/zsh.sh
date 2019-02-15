cf52028d70251267da30fd837d2baa791361982f
d-salt <d.salt.web+dev@gmail.com>
2019-02-15 11:13:20 +0900

    create shell configuration

---
+++ etc/init/common/zsh.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/env bash
+
+set -Cue
+
+."$DOTPATH"/etc/ilb/vital.sh
+
+
+if ! has "zsh"; then
+    echo "Error: require: zsh"
+    exit 1;
+fi
+
+if ! contains "${SHELL:-}" "zsh"; then
+    zsh_path="${which zsh}"
+
+    if ! grep -xq "${zsh_path:=/bin/zsh}" /etc/shells; then
+        echo "Error: you should append '$zsh_path' to /etc/shells"
+        exit 1
+    fi
+
+    if [ -x "$zsh_path" ]; then
+        if chsh -s "$zsh_path" "${USER:-root}"; then
+            echo "Done: Change shell to '$zsh_path' for '${USER:-root}' successfully"
+        else
+            echo "Error: cannnot set '$zsh_path' as \$SHELL"
+            echo "       Is '$zsh_path' descrived in /etc/shells?"
+            echo "       you should run 'chsh -l' now"
+            exit 1
+        fi
+
+    else
+        echo "Error: '$zsh_path': invalid path"
+        exit 1
+    fi
+fi
+
