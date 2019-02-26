760bb1bc23a32fd4857c04f3340e612719030b01
d-salt <d.salt.web+dev@gmail.com>
2019-02-26 15:33:30 +0900

    update

--- etc/init/osx/20_emacs_mozc.sh
+++ etc/init/osx/20_emacs_mozc.sh
@@ -33,6 +33,7 @@ MOZCPATH="${DOTPATH}/etc/mozc"
 
         if ! has ninja; then
             die "ninja: Faild to install."
+        fi
     fi
 }
 
@@ -50,15 +51,18 @@ MOZCPATH="${DOTPATH}/etc/mozc"
     SDK_VER="$(ls /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs | grep -E "[0-9]+\.sdk$" | sed -E 's/^MacOSX([0-9\.]+)\.sdk$/\1/')"
     GYP_DEFINES="mac_sdk=${SDK_VER} mac_deployment_target=${SW_VER}" python build_mozc.py gyp --noqt
 
-    e_info "Please change build terget by your self. \n Select Deployment Target (your sdk ver) on `Interface Builder Document > Document Editing > Builds for`"
-    open -W -aa /Applications/Xcode.app ./third_party/breakpad/src/client/mac/sender/Breakpad.xib
+    e_info "Please change build terget by your self."
+    e_info "Select Deployment Target (your sdk ver) on \`Interface Builder Document > Document Editing > Builds for\`"
+    e_info "Then \`quit\` Xcode.app to return script."
+    open -W -a /Applications/Xcode.app ./third_party/breakpad/src/client/mac/sender/Breakpad.xib
     
     python build_mozc.py build -c Release mac/mac.gyp:GoogleJapaneseInput mac/mac.gyp:gen_launchd_confs unix/emacs/emacs.gyp:mozc_emacs_helper
 
     e_arrow 'Installing mozc'
     sudo cp -r out_mac/Release/Mozc.app /Library/Input\ Methods/
-    sudo cp out_mac/DerivedSources/Release/mac/org.mozc.inputmethod.Japanese.Converter.plist /Library/LaunchAgents
-    sudo cp out_mac/DerivedSources/Release/mac/org.mozc.inputmethod.Japanese.Renderer.plist /Library/LaunchAgents
+    sudo cp out_mac/Release/gen/mac/org.mozc.inputmethod.Japanese.Converter.plist /Library/LaunchAgents
+    sudo cp out_mac/Release/gen/mac/org.mozc.inputmethod.Japanese.Renderer.plist /Library/LaunchAgents
+    sudo cp out_mac/Release/mozc_emacs_helper /usr/local/bin/
 }
 
 e_success "emacs_mozc: Installed successfully!"
