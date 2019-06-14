9274379ecbc1dece7fa4fc430feefaa0d552118a
d-salt <d.salt.web+dev@gmail.com>
2019-06-14 17:47:32 +0900

    install latest version by source build: emacs & tmux

---
+++ etc/init/linux/11_emacs.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/env bash
+
+set -Cue
+
+if [ -z "$DOTPATH" ]; then
+    echo '$DOTPATH not set' >&2
+    exit 1
+fi
+
+. ${DOTPATH}/etc/lib/vital.sh
+
+if has "yum"; then
+    e_arrow "Install emacs25 by Source Build."
+    sudo yum -y install ncurses-devel
+    curl -LO http://ftp.jaist.ac.jp/pub/GNU/emacs/emacs-25.3.tar.xz
+    tar xzvf emacs-25.3.tar.xz
+    cd emacs-25.3
+    ./configure
+    make
+    sudo make install
+elif has "apt"; then
+    e_arrow "Install emacs25 via PPA with Advanced Packaging Tool."
+    sudo apt -y install ncurses-dev
+    sudo add-apt-repository ppa:kelleyk/emacs
+    sudo apt update
+    sudo apt -y install emacs25
+elif has "apk"; then
+else
+    die "require: yum or apt"
+fi
+
+e_success "emacs: Installed successfully."
+exit 0
+
