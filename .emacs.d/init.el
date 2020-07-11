72783e2fffdc74367c98f25d40a9cecd5d2a5929
d-salt <d.salt.web+dev@gmail.com>
2020-07-11 23:19:31 +0900

    update whitespace configs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -133,17 +133,20 @@
   :init
   (global-whitespace-mode 1)
   :config
-  (setq whitespace-style '(face tabs tab-mark spaces space-mark))
+  (setq whitespace-style '(face trailing tabs tab-mark spaces space-mark))
   (setq whitespace-display-mappings
         '((space-mark ?\u3000 [?\u25a1])
           (tab-mark ?\t [?\xBB ?\t] [?\\ ?\t])))
   (setq whitespace-space-regexp "\\(\u3000+\\)")
-  (set-face-foreground 'whitespace-tab "#adff2f")
-  (set-face-background 'whitespace-tab 'nil)
-  (set-face-underline  'whitespace-tab t)
-  (set-face-foreground 'whitespace-space "#7cfc00")
-  (set-face-background 'whitespace-space 'nil)
-  (set-face-bold-p     'whitespace-space t))
+  (set-face-attribute 'whitespace-trailing nil
+                      :foreground "#7cfc00"
+                      :underline t)
+  (set-face-attribute 'whitespace-space nil
+                      :foreground "#7cfc00"
+                      :weight 'bold)
+  (set-face-attribute 'whitespace-tab nil
+                      :foreground "#adff2f"
+                      :underline t))
 
 (use-package sr-speedbar
   :config
